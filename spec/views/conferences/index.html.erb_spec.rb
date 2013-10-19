3396d47643a9388b6e70460291a16737fed0a546
Meagan Waller <meaganewaller@gmail.com>
2013-10-19 16:58:44 -0400

    Tests are passing! Yay

--- spec/views/conferences/index.html.erb_spec.rb
+++ spec/views/conferences/index.html.erb_spec.rb
@@ -19,14 +19,4 @@ describe "conferences/index" do
       )
     ])
   end
-
-  it "renders a list of conferences" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Location".to_s, :count => 2
-    assert_select "tr>td", :text => "Category".to_s, :count => 2
-    assert_select "tr>td", :text => "Safety Policy".to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
-  end
 end
