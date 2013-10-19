8f9e9e3a4698df1480caa542b8a549c9b6406688
Gavin Stark <gavin@gstark.com>
2013-10-19 19:47:28 -0400

    Better scope name

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -8,7 +8,7 @@ class Conference < ActiveRecord::Base
   geocoded_by :location
   after_validation :geocode, :if => lambda { location_changed? && (latitude.blank? || longitude.blank?) }
 
-  scope :between_dates, lambda { |date| where("start_date <= ? and end_date >= ?", date, date) }
+  scope :on_date, lambda { |date| where("start_date <= ? and end_date >= ?", date, date) }
 
   validates :safety_policy, :format => { with: URI::regexp(%w(http https)), :message => "must be a URL to your policy" }, allow_blank: true
 
@@ -27,7 +27,7 @@ class Conference < ActiveRecord::Base
       childcare:     where({childcare: ActiveRecord::ConnectionAdapters::Column.value_to_boolean(fields[:childcare])}),
       safety_policy: where("safety_policy is not null and safety_policy <> ''"),
       location:      near(fields[:location]),
-      date:          between_dates(fields[:date]),
+      date:          on_date(fields[:date]),
     }
 
     scopes.keys.inject(self.all) do |results, field|
