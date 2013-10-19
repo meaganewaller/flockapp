71ee16f3f928adb99188f6941ca2f50345fa7e91
Meagan Waller <meaganewaller@Meagans-MacBook-Pro.local>
2013-10-18 20:48:36 -0400

    Create initial Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flockapp::Application.config.secret_key_base = 'c95f31d008e715758c4ad41c04ca9961c621c466844ec627c7f783e3708f7fbc310af83a131485543981a1ade720e48d31637b27a5f0bdfbdaa2dbd427385e67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flockapp::Application.config.session_store :cookie_store, key: '_flockapp_session'
