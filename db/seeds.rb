6ff1675e68b2f3e62bed2bb29c8db7c066f115a8
Chong Kim <chongkim@yahoo.com>
2013-10-19 14:15:22 -0400

    adds airport seed data and region model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# populate regions
+regions = {}
+CSV.parse(File.read(File.expand_path("../csv/regions.csv", __FILE__)), :headers => true).each do |row|
+  h = row.to_hash
+  Region.create!(:name => h["name"], :code => h["code"], :continent => h["continent"], :iso_country => h["iso_country"])
+  regions[h["code"]] = h["name"]
+end
+
+
+# populate airports
+csv = CSV.parse(File.read(File.expand_path("../csv/airports.csv", __FILE__)), :headers => true)
+count = 0
+csv.each do |row|
+  r = row.to_hash
+  Airport.create!(:name => r["name"], :code => r["local_code"], :location => "#{r["municipality"]} #{regions[r["iso_region"]]} #{r["iso_country"]}", :latitude => r["latitude_deg"], :longitude => r["longitude_deg"])
+  count += 1
+  puts "#{count} / 45378" if count % 1000 == 0
+end
