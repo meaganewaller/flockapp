199211a560cab4258d43ac23ee84b1a4ebf7a315
Gavin Stark <gavin@gstark.com>
2013-10-19 12:17:33 -0400

    Fixes welcome controller spec with new "coming_soon" default page

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,7 +4,7 @@ describe WelcomeController do
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
+      get 'coming_soon'
       response.should be_success
     end
   end
