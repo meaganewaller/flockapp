2eedbb3327d2d52bd934abcb3fe59132c85ab68b
Gavin Stark <gavin@gstark.com>
2013-10-20 13:37:49 -0400

    Adds formatting of the conference list page and some conference seed data

--- app/helpers/conferences_helper.rb
+++ app/helpers/conferences_helper.rb
@@ -1,2 +1,9 @@
 module ConferencesHelper
+  def by_month(conferences)
+    conferences.group_by { |conference| conference.start_date ? conference.start_date.strftime("%B %Y") : "" }
+  end
+
+  def start_day_of_conference(conference)
+    conference.start_date ? conference.start_date.day : ""
+  end
 end
