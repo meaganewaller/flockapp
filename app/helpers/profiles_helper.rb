2d3f5c81dcfd07bd30e85268cdc86227ccc8855f
Meagan Waller <meaganewaller@gmail.com>
2013-10-20 16:03:44 -0400

    Profiles styling

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,6 +1,6 @@
 module ProfilesHelper
   def gravatar_for_profile(user)
-    gravatar_image_tag(user.email.downcase, :alt => user.name, :class => "gravatar-profile", :gravatar => {:size => 300 })
+    gravatar_image_tag(user.email.downcase, :alt => user.name, :class => "gravatar-profile", :gravatar => {:size => 200 })
   end
 end
 
